bfd0662d2a13f8bd4abd4721fa164cd3e681e10c
hcalldee <hcalldee@gmail.com>
2024-07-20 12:04:58 +0800

    push final

--- src/js/pages/FinishMenu/FinishMenu.jsx
+++ src/js/pages/FinishMenu/FinishMenu.jsx
@@ -2,22 +2,36 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Fade } from '../../transitions/Fade/Fade';
 import { Button } from '../../components/atoms/Button';
+import { ButtonIcon } from '../../components/atoms/ButtonIcon';
 
 export const FinishMenu = () => {
 	const navigate = useNavigate();
 
 	return (
-		<Fade className="flex flex-col items-center justify-center w-full h-screen gap-8 rounded-lg md:flex-row">
-			<div className="flex flex-col items-center justify-center w-full gap-14">
-				<div className="flex flex-col items-center w-full gap-4 md:gap-8">
-					<img src={require('../../../assets/okay-ok.gif')} alt="" className="w-64 rounded-lg shadow-md lg:w-80" />
-					<hr />
-					<div className="flex flex-col items-center gap-2 mt-10 md:gap-8 md:flex-row md:mt-5">
-						<Button label="Halaman Utama" className="w-56" size="xl" onClick={() => navigate('/')} />
-						<Button label="Tentang Kreator" className="w-56" size="xl" onClick={() => navigate('/author')} />
-					</div>
-				</div>
+		<div className="h-screen w-full flex items-center justify-center">
+		  <Fade className="flex flex-col items-center justify-center w-full h-full gap-8 rounded-lg md:flex-row">
+			{/* <audio ref={openingRef} src={require('../../../assets/opening.wav')} onEnded={handleEnded} /> */}
+	
+			<div className="flex flex-col items-center justify-center w-full h-full">
+			  <img className="z-0 w-full h-full object-cover rounded-lg shadow-xl" src={require('../../../assets/cover-end.jpg')} alt="" />
+
+			  <Button label="Halaman Utama"
+				className="absolute w-35"
+				// icon={require('../../../assets/start.png')}
+					onClick={() => navigate('/')}
+				style={{ top: '85%', left: '42%', transform: 'translate(-50%, -50%)' }} // Center the button icon
+				/>
+
+			  <Button label="Tentang Kreator"
+				className="absolute w-35"
+				// icon={require('../../../assets/start.png')}
+					onClick={() => navigate('/author')}
+				style={{ top: '85%', left: '58%', transform: 'translate(-50%, -50%)' }} // Center the button icon
+				/>
+				
+
 			</div>
-		</Fade>
+		  </Fade>
+		</div>
 	);
 };
